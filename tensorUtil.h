4eb4d6b9932ae8a935063a2cb2f9999d80130470
zhixu <zzx.2013@qq.com>
2017-11-12 08:18:03 +0800

    finish printTensor

--- tensorUtil.h
+++ tensorUtil.h
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <cuda_runtime.h>
-#include <helper_functions.h>
-#include <helper_cuda.h>
+/* #include <cuda_runtime.h> */
+/* #include <helper_functions.h> */
+/* #include <helper_cuda.h> */
 
 #define MAXDIM 8
 
 typedef struct {
      int ndim;
-     int dims[MAXDIM];
+     int *dims;
      int len;
      float *data;
 } Tensor;
 
 int computeLength(int *dims, int ndim);
-Tensor createTensor(float *data, int ndim, int *dims, int len);
-int printTensor(Tensor *tensor);
+Tensor createTensor(float *data, int ndim, int *dims);
+int printTensor(Tensor *tensor, const char *fmt);
 void sliceTensor(Tensor *src, Tensor *dst, int dim, int start, int len);
